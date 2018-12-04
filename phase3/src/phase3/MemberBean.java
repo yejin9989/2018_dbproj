cdca628ae90f5555fb4fd0cedd7036c633d96a3d
yoooooola <39353959+yoooooola@users.noreply.github.com>
2018-12-05 02:38:16 +0900

    Add files via upload

---
+++ phase3/src/phase3/MemberBean.java
@@ -0,0 +1,16 @@
+package phase3;
+
+public class MemberBean {
+	private int orderNum;
+	private String pitem;
+	private int pquantity;
+	
+	public int getOrdernum() {return orderNum;}
+	public void setOrdernum(int orderNum) {this.orderNum = orderNum;}
+	
+	public String getPitem() {return pitem;}
+	public void setPitem(String pitem) {this.pitem = pitem;}
+	
+	public int getPquantity() {return pquantity;}
+	public void setPquantity(int pquantity) {this.pquantity = pquantity;}
+}
